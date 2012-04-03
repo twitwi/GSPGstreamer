7e706e68f435b586be812376d771b150aff5e2d1
Remi Emonet <remi.emonet@reverse--com.heeere>
2012-04-03 18:38:29 +0200

    added an overlay to the swing view (non-gsp), making a copy as my ByteBufferDataBuffer does not support writing (yet?)

--- src/main/java/com/heeere/gsp/gstreamer/AppUseModuleWithoutPipeline.java
+++ src/main/java/com/heeere/gsp/gstreamer/AppUseModuleWithoutPipeline.java
@@ -1,6 +1,9 @@
 package com.heeere.gsp.gstreamer;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -33,12 +36,23 @@ public class AppUseModuleWithoutPipeline {
                 System.err.println("last i: " + i);
                 break;
             }
-            imageIcon.setImage(source.getLastOutput());
+            {
+                BufferedImage disp = source.getLastOutput(); // there is no copy here but there was already a copy in the ImageSource
+                disp = new BufferedImage(disp.getWidth(), disp.getHeight(), BufferedImage.TYPE_3BYTE_BGR);
+                Graphics2D g = disp.createGraphics();
+                g.drawImage(source.getLastOutput(), 0, 0, null);
+                g.setColor(Color.RED);
+                g.translate(200., 100.);
+                g.rotate(i*.1);
+                g.scale(2., 2.);
+                g.drawString("Frame "+i, -20f, 0f);
+                imageIcon.setImage(disp);
+            }
             imageView.repaint();
             if (i % 100 == 0) {
                 System.err.println(i);
             }
-            Thread.sleep(50);
+            //Thread.sleep(20);
         }
         System.err.println("duration: " + (System.currentTimeMillis() - t) + "ms");
     }
